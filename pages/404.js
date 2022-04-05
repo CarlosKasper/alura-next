2a0a4607e085920c8702a0abdf1200a11f187234
Carlos Eduardo Kasper <52386569+CarlosKasper@users.noreply.github.com>
2022-04-05 14:17:24 -0300

    keep learning my friend

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link";
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>Você se perdeu!</h1>
+      <Link href='/'>
+        Acessar a HOME
+      </Link>
+    </div>
+  )
+}
