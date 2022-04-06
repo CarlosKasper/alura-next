6861c5b0a0710c37a82ee200154a26b6f4de0dc8
Carlos Eduardo Kasper <52386569+CarlosKasper@users.noreply.github.com>
2022-04-06 10:24:14 -0300

    update faq page

--- pages/faq.js
+++ pages/faq.js
@@ -2,21 +2,6 @@ import Head from "next/head";
 import Link from "../src/components/Link";
 import PageTitle from "../src/components/PageTitle";
 
-export async function getStaticProps() { // yarn build && yarn export, funciona
-  // roda a função somente no build time
-  const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
-  const faq = await fetch(FAQ_API_URL)
-      .then((response) => { return response.json();})
-      .then((data) => {
-        return data;
-      });
-
-  return {
-    props: {
-      faq: faq,
-    },
-  };
-}
 
 // export async function getServerSideProps() { // yarn export nao funciona, entao deve ser rodado yarn build && yarn start
 //   // roda a função a cada acesso que recebe
@@ -34,27 +19,27 @@ export async function getStaticProps() { // yarn build && yarn export, funciona
 //   };
 // }
 
-export default function FAQPage({ faq }) {
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
-  return (
-    <div>
-      <PageTitle>
-        FAQ - Alura Cases Campanha
-      </PageTitle>
-      <h1>Alura Cases - Página de perguntas FAQ</h1>
-      <Link href='/'>
-        Acessar a HOME
-      </Link>
-      <ul>
-        {faq.map(({ question, answer }) => (
-          <li key={question}>
-            <article>
-              <h5>{question}</h5>
-              <p>{answer}</p>
-            </article>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-}
+export default FAQScreen; 
+
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
+
+    return {
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
